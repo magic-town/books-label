b7e7efc5644f41413f9573b168420725fa76818e
gabrielfernando01 <fernando.rozepeda@gmail.com>
2026-03-15 12:31:30 -0600

    sync 2026-03-15 12:31 | venv_catalogo/bin/activate,venv_catalogo/bin/activate.csh,venv_catalogo/bin/activate.fish,venv_catalogo/bin/f2py,venv_catalogo/bin/normalizer

--- venv_catalogo/bin/activate.fish
+++ venv_catalogo/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/sonia/books-label/venv_catalogo
+set -gx VIRTUAL_ENV /home/gabriel/books-label/venv_catalogo
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
